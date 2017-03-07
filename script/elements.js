adea38e07c32b4b1816c5410fdebcbac1dae9af5
Yroki12/Loc <yutan0926naughty@gmail.com>
2017-03-08 00:21:28 +0900

    GUIとDataに必要な変数

---
+++ script/elements.js
@@ -0,0 +1,76 @@
+const modName = ".invasion";
+
+const Thread=java.lang.Thread;
+const Runnable=java.lang.Runnable;
+const R=android.R;
+
+const Base64=android.util.Base64;
+const Bitmap=android.graphics.Bitmap;
+const BitmapFactory=android.graphics.BitmapFactory;
+const BitmapDrawable=android.graphics.drawable.BitmapDrawable;
+const Rect=android.graphics.Rect;
+const Paint=android.graphics.Paint;
+const Canvas=android.graphics.Canvas;
+const Color=android.graphics.Color;
+
+const NinePatchDrawable=android.graphics.drawable.NinePatchDrawable;
+const LayerDrawable=android.graphics.drawable.LayerDrawable;
+const ClipDrawable=android.graphics.drawable.ClipDrawable;
+const AnimationDrawable=android.graphics.drawable.AnimationDrawable;
+const StateListDrawable=android.graphics.drawable.StateListDrawable;
+const Shader=android.graphics.Shader;
+const Matrix=android.graphics.Matrix;
+const ColorMatrix=android.graphics.ColorMatrix;
+const ColorMatrixColorFilter=android.graphics.ColorMatrixColorFilter;
+const ColorDrawable=android.graphics.drawable.ColorDrawable;
+const ShapeDrawable=android.graphics.drawable.ShapeDrawable;
+const ColorStateList=android.content.res.ColorStateList;
+
+const Button=android.widget.Button;
+const ImageButton=android.widget.ImageButton;
+const ToggleButton=android.widget.ToggleButton;
+const Switch=android.widget.Switch;
+const CheckBox=android.widget.CheckBox;
+const OnClickListener=android.view.View.OnClickListener;
+const OnTouchListener=android.view.View.OnTouchListener;
+const MotionEvent=android.view.MotionEvent;
+const OnSeekBarChangeListener=android.widget.SeekBar.OnSeekBarChangeListener;
+const OnCheckedChangeListener=android.widget.CompoundButton.OnCheckedChangeListener;
+const LinearLayout=android.widget.LinearLayout;
+const RelativeLayout=android.widget.RelativeLayout;
+const FrameLayout=android.widget.FrameLayout;
+const ScrollView=android.widget.ScrollView;
+const ImageView=android.widget.ImageView;
+const TextView=android.widget.TextView;
+const PopupWindow=android.widget.PopupWindow;
+const EditText=android.widget.EditText;
+const SeekBar=android.widget.SeekBar;
+const ProgressBar=android.widget.ProgressBar;
+const RadioButton=android.widget.RadioButton;
+
+const View=android.view.View;
+const ViewGroup=android.view.ViewGroup;
+const Gravity=android.view.Gravity;
+
+const DisplayMetrics=android.util.DisplayMetrics;
+
+const Environment=android.os.Environment;
+
+const File=java.io.File;
+const OutputStreamWriter=java.io.OutputStreamWriter;
+const FileInputStream=java.io.FileInputStream;
+const FileOutputStream=java.io.FileOutputStream;
+const FileWriter=java.io.FileWriter;
+const FileReader=java.io.FileReader;
+const BufferedWriter=java.io.BufferedWriter;
+const BufferedReader=java.io.BufferedReader;
+
+const TGALoader=net.zhuoweizhang.mcpelauncher.texture.tga.TGALoader;
+
+const ScriptManager=net.zhuoweizhang.mcpelauncher.ScriptManager;
+const ScriptableObject=org.mozilla.javascript.ScriptableObject;
+
+const jString=java.lang.String;
+
+const ByteBuffer=java.nio.ByteBuffer;
+const ByteOrder=java.nio.ByteOrder;
